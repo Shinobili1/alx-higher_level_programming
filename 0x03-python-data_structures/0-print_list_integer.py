00ec6386dae26dc171f47de97d9aa7ebe1f84984
Biliyoung Aishino <biliyounga@gmail.com>
2022-09-26 10:10:36 -0700

    data structure

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,1 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-for n in range(len(my_list)):
-print("{:d}".format(my_list[n]))
+#!/usr/bin/python3 def print_list_integer(my_list=[]): for index in my_list: print("{:d}".format(index))
